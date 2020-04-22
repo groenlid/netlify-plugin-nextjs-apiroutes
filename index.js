fd345d1a57be137e0cc1ecd83396bbd39f96fb32
Mats Groenlid <mats.gronlid@bouvet.no>
2020-04-22 20:10:00 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import { promises as fs } from "fs"
+const { appendFile, writeFile, readFile } = fs
+
+const nextPath = ".next"
+const manifestPath = `${nextPath}/pages-manifest.json`
+
+async function readPagesManifest() {
+    try {
+        const manifest = await readFile(manifestPath, { encoding: 'utf-8'})
+        return JSON.parse(manifest);
+    } catch(err){
+        if(err.code === 'ENOENT') return
+        throw err
+    }
+}
+
+function getApiRoutes(manifest) {
+    return Object.fromEntries(manifest.entries(manifest).filter(([key, _]) => key.startsWith('/api')))
+}
+
+function netlifyPlugin(conf) {
+    return {
+        name: 'netlify-plugin-nextjs-apiroutes',
+        onPackage: async () => {
+            console.log('Find nextjs pages-manifest.json')
+            const manifest = await readPagesManifest()
+            if(!manifest) {
+                console.log('No nextjs pages-manifest file found. Will exit early')
+                return
+            }
+            console.log('Read through to find all api routes')
+            const apiRoutes = getApiRoutes(manifest)
+            console.log('Add rewrite rule to redirect file')
+            console.log('Add a new handler to handle the nextjs api route')
+        }
+    }
+}
+
+module.exports = netlifyPlugin
